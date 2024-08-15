4b235686f66994df7344bb5ce6ed8105382b9d07
fsd-abdulazis-12001 <abanggaul111@gmail.com>
2024-08-15 23:36:54 +0700

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -34,6 +34,7 @@ app.use(errorHandler)
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
 });
+module.exports = app;
 // const handler = ServerlessHttp(app);
 // module.exports.handler = async (event, context) => {
 //     const result = await handler(event, context);
